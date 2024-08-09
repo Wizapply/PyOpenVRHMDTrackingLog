ad76d24df0735c3d72328faf173c6eedc3b325e5
WIZAPPLY <info@wizapply.com>
2024-08-09 11:04:30 +0900

    Bug Fixed

--- PyOpenVRHMDTrackingLog.py
+++ PyOpenVRHMDTrackingLog.py
@@ -7,10 +7,7 @@ import numpy as np
 
 class PyOpenVRHMDTrackingLog:
 
-    GET_DEVICE_NUM = 5
-    LOGGER_DEVCICE = openvr.k_unTrackedDeviceIndex_Hmd
-
-    def __init__(self):
+    def __init__(self, tracked_device = 0):
         self.Status = False
         self.ZeroAbsolute_tk_pose = np.identity(4)
         self.Absolute_tk_pose_np = self.ZeroAbsolute_tk_pose
@@ -18,7 +15,7 @@ class PyOpenVRHMDTrackingLog:
         self.Rotation = (0, 0, 0)
 
         try:
-            openvr.init(openvr.VRApplication_Background)
+            openvr.init(openvr.VRApplication_Other)
         except Exception:
             raise Exception('Error: SteamVR launch failed!')
         
@@ -46,28 +43,32 @@ class PyOpenVRHMDTrackingLog:
         if not self.Status:
             return False
 
-        all_poses = openvr.VRSystem().getDeviceToAbsoluteTrackingPose(openvr.TrackingUniverseStanding, 0, self.GET_DEVICE_NUM)
+        all_poses = openvr.VRSystem().getDeviceToAbsoluteTrackingPose(openvr.TrackingUniverseStanding, 0, openvr.k_unMaxTrackedDeviceCount)
         if len(all_poses) < 1:
             return False
-    
-        tracker_pose = all_poses[self.LOGGER_DEVCICE]
-        if tracker_pose.eTrackingResult == openvr.TrackingResult_Running_OK and tracker_pose.bDeviceIsConnected == True:
-            absolute_tk_pose = tracker_pose.mDeviceToAbsoluteTracking
-            absolute_tk_pose3 = [0.0, 0.0, 0.0, 1.0]
-            self.Absolute_tk_pose_np = np.matrix([absolute_tk_pose[0], absolute_tk_pose[1], absolute_tk_pose[2], absolute_tk_pose3])
-        
-            absolute_tk_pose_np_dot = np.dot(self.Absolute_tk_pose_np, self.ZeroAbsolute_tk_pose)
-            self.Position = self._toPosition(absolute_tk_pose_np_dot, rd)
-            self.Rotation = self._toRotationAngles(absolute_tk_pose_np_dot, rd)
 
-            return True
+        tracker_pose_res = None
+        for tracker_pose in all_poses:           
+            if tracker_pose.eTrackingResult == openvr.TrackingResult_Running_OK and tracker_pose.bDeviceIsConnected == True:
+                absolute_tk_pose = tracker_pose.mDeviceToAbsoluteTracking
+                absolute_tk_pose3 = [0.0, 0.0, 0.0, 1.0]
+                self.Absolute_tk_pose_np = np.matrix([absolute_tk_pose[0], absolute_tk_pose[1], absolute_tk_pose[2], absolute_tk_pose3])
 
-        return False
+                absolute_tk_pose_np_dot = np.linalg.inv(self.ZeroAbsolute_tk_pose) @ self.Absolute_tk_pose_np
+                self.Position = self._toPosition(absolute_tk_pose_np_dot, rd)
+                self.Rotation = self._toRotationAngles(absolute_tk_pose_np_dot, rd)
+                tracker_pose_res = tracker_pose
+                break
+
+        if tracker_pose_res is None:
+            return False
+
+        return True
 
     def ResetZeroPose(self):
         if not self.Status:
             return False
-        self.ZeroAbsolute_tk_pose = np.linalg.inv(self.Absolute_tk_pose_np)
+        self.ZeroAbsolute_tk_pose = self.Absolute_tk_pose_np
         return True
     
     def GetPosition(self):
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 ﻿#HMD HeadTracking to CSV
-# -*- coding: utf-8 -*-
+#req: pip install openvr
+
 import msvcrt, csv
-import time
+import time, socket
 import PyOpenVRHMDTrackingLog
 
 from ctypes import windll
@@ -14,56 +15,70 @@ def kbHit():
         kb = ord(msvcrt.getch())
         if kb == 0x00:
             kb2 = ord(msvcrt.getch())
-            if kb2 == 0x43:
+            if kb2 == 0x43: #F9
                 result = 1
-        if kb == 0x1B:
-            result = 2
+            if kb2 == 0x44: #F10
+                result = 2
+        if kb == 0x1B:      #ESC
+            result = 3
     return result
 
 # ------- main -------
 
-print("HMD HeadTracking to CSV")
-print("  (C)WIZAPPLY CO.,LTD.")
+print('HMD Vive Tracking to CSV')
+print('  (C)WIZAPPLY CO.,LTD.')
 
 try:
     svhl = PyOpenVRHMDTrackingLog.PyOpenVRHMDTrackingLog()
 except Exception as e:
-    print("Error: SteamVR Application launch failed!")
+    print('Error: SteamVR Application launch failed!')
     exit()
 
-csv_file_w = open('./test_data.csv', 'w', encoding='utf_8_sig', errors='', newline='')
-dataf_w = csv.writer(csv_file_w, delimiter=',', doublequote=True, lineterminator='\r\n', quotechar='"', skipinitialspace=True)
-dataf_w.writerow(['Time(s)','PosX(m)','PosY(m)','PosZ(m)','RotX(deg)','RotY(deg)','RotZ(deg)'])
+csv_file_w = None
+tracking_data_csv_path = './tracking_data.csv'
 
-print("Tracking Start....")
-print("[F9]Key: RESET ZERO POSE")
+print('Tracking Start....')
+print('[F9]Key: RESET ZERO POSE')
+print('[F10]Key: Start&Stop SAVE DATA to CSV files')
 
 #time
 time_sta = time.time()
 
 windll.winmm.timeBeginPeriod(1)
 while True:
-    if svhl.UpdatePose(5):
+    if svhl.UpdatePose(6):
         #show
         position = svhl.GetPosition()
         rotation = svhl.GetRotation()
-        curTime = (time.time() - time_sta, )
+        curTime = time.time() - time_sta
 
-        viewData = round(curTime,4) + position + rotation
-        print(viewData) #debug
-        dataf_w.writerow(viewData) #save
+        viewData = (round(curTime,4), position[0],position[1],position[2])
+        #print(viewData) #debug
+        if not csv_file_w is None and not csv_file_w.closed:
+            dataf_w.writerow(viewData) #save
 
     res = kbHit()
-    if res == 1:
+    if res == 1:    #f9
         svhl.ResetZeroPose()
-        print("RESET ZERO POSE")
-    if res == 2:
+        print('RESET ZERO POSE')
+    if res == 2:    #f10
+        if csv_file_w is None or csv_file_w.closed:
+            csv_file_w = open(tracking_data_csv_path, 'w', encoding='utf_8_sig', errors='', newline='')
+            dataf_w = csv.writer(csv_file_w, delimiter=',', doublequote=True, lineterminator='\r\n', quotechar='"', skipinitialspace=True)
+            dataf_w.writerow(['Time(s)','PosX(m)','PosY(m)','PosZ(m)','RotX(deg)','RotY(deg)','RotZ(deg)'])
+            print('START RECORD DATA TO CSV FILE. -> ./tracking_data.csv')
+        else:
+            csv_file_w.close()
+            csv_file_w = None
+            print('TRACKING DATA WAS RECORDED.')
+    if res == 3:    #end
         break
 
     time.sleep(.01) #10ms
 
 svhl.Close()
-csv_file_w.close()
+if not csv_file_w is None and not csv_file_w.closed:
+    csv_file_w.close()
 windll.winmm.timeEndPeriod(1)
 
-print("FINISH: Save CSV")
+print('FINISH TRACKER.')
